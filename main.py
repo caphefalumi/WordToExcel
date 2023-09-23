50113916ee85ffa9671da26f84a6e59998237d51
Dangduytoan12l <59313808+Dangduytoan12l@users.noreply.github.com>
2023-09-24 01:22:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import win32com.client
 from docx.enum.text import WD_COLOR_INDEX as color
 import docx.enum.text
-
+from init import *
 
 
 # Open the Word document
@@ -14,31 +14,6 @@ current_question = ""
 options = []
 highlights = []
 
-
-def extract_highlighted_text(paragraph):
-    highlighted_text = ""
-    for run in paragraph.runs:
-        if run.font.highlight_color:  # RGB for yellow
-            highlighted_text += run.text
-    return highlighted_text
-
-def get_correct_answer_index(options, highlights):
-    for i, option_text in enumerate(options):
-        if option_text in highlights:
-            return i+1
-    return None
-
-def close_excel():
-    file_path = r"C:\Users\Toan\WordToExcel\questions.xlsx"
-    try:
-        excel = win32com.client.Dispatch("Excel.Application")
-        excel.Visible = False  # Optional: Hide Excel window
-        workbook = excel.Workbooks.Open(file_path)
-        workbook.Close(True)  # True to save changes, False to discard changes
-        excel.Quit()
-        print(f'Closed Excel file: {file_path}')
-    except Exception as e:
-        print(f'Error: {str(e)}')
 
 # Iterate through the document to extract highlighted text
 for paragraph in doc.paragraphs:
