afc7bef1b17fd97819770b24cc66cd8442e7bd37
Dangduytoan12l <dangduytoan13l@gmail.com>
2023-09-30 23:58:52 +0700

    Fix alignments

--- gui.py
+++ gui.py
@@ -29,28 +29,29 @@ main_frame.pack(pady=20)
 
 # Header label
 header_label = tk.Label(main_frame, text="Convert Word to Excel", font=("Helvetica", 16))
-header_label.grid(row=0, column=0, columnspan=2, pady=10)
+header_label.grid(row=0, column=0, columnspan=3, pady=10, sticky="n")  # Center the label using "sticky"
 
 # File selection button
 file_button = tk.Button(main_frame, text="Select Word Document", command=run)
-file_button.grid(row=1, column=0, columnspan=2, pady=10)
+file_button.grid(row=1, column=0, columnspan=3, pady=10, sticky="n")  # Center the button using "sticky"
 
 # Platform radio buttons
 platform_options = ["Quizizz", "Kahoot", "Blooket"]
 platform_selection = tk.StringVar(window)
 platform_selection.set(platform_options[0])  # Set the default value
+
 platform_quizizz = tk.Radiobutton(main_frame, text="Quizizz", variable=platform_selection, value="Quizizz")
 platform_kahoot = tk.Radiobutton(main_frame, text="Kahoot", variable=platform_selection, value="Kahoot")
 platform_blooket = tk.Radiobutton(main_frame, text="Blooket", variable=platform_selection, value="Blooket")
 
-
-# Place the radio buttons below the Select Word Document button
-platform_quizizz.grid(row=2, column=0, pady=10)
-platform_kahoot.grid(row=2, column=1, pady=10)
+# Place the radio buttons in the center below the Select Word Document button
+platform_quizizz.grid(row=2, column=0, pady=10, sticky="n")
+platform_kahoot.grid(row=2, column=1, pady=10, sticky="n")
+platform_blooket.grid(row=2, column=2, pady=10, sticky="n")
 
 # Status label
 status_label = tk.Label(main_frame, text="", fg="green")
-status_label.grid(row=3, column=0, columnspan=2, pady=10)
+status_label.grid(row=3, column=0, columnspan=3, pady=10, sticky="n")  # Center the label using "sticky"
 
 # Start the GUI application
 window.mainloop()
