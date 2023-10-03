ebc74b93e9f5f1bad8f59229b8e423e7bc626881
Dangduytoan12l <dangduytoan13l@gmail.com>
2023-10-03 15:00:15 +0700

    Update .bat file

--- scripts.bat
+++ scripts.bat
@@ -1,11 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
+rem Install Chocolatey
+echo Installing Chocolatey...
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
 rem Define the Python version to install
 set PYTHON_VERSION=3.9.7
 
 rem Define the list of Python libraries to install
-set LIBRARIES=pandas python-docx regex tk pywin32 tkinter
+set LIBRARIES=pandas python-docx regex tk pywin32
 
 rem Install Python
 echo Installing Python %PYTHON_VERSION%...
@@ -16,13 +20,16 @@ python --version
 
 rem Install Python libraries
 echo Installing Python libraries...
-pip install --upgrade pip
 for %%i in (%LIBRARIES%) do (
     echo Installing %%i...
     pip install %%i
 )
 
 echo Installation completed.
+
+rem Run the Python script
+echo Running gui.py...
+python gui.py
 
 rem Pause to keep the command prompt open
 pause
